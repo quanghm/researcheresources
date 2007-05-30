75de40a5301e662fc2a092b2b9e8ef2f2b31cb75
kimcuong <kimcuong@2dacef61-802d-0410-83c8-2ff38f27e8b0>
2007-05-30 06:59:56 +0000

    email works

--- trunk/VEF/PaperShare/test/sendmail/sendmail.php
+++ trunk/VEF/PaperShare/test/sendmail/sendmail.php
@@ -1,9 +1,10 @@
 <?php
-$email = $_GET['email'] ;
+$emailFrom = $_GET['emailFrom'] ;
+$emailTo = $_GET['emailTo'] ;
 $message = $_GET['message'] ;
-echo "<p> Received <p> $message <p> from $email\n";
-if ( mail( "kimcuong@gmail.com", "Email Subject : Test", $message, "From: $email" ) ) {
-    echo "<p> Congratulations your email has been sent";
+echo "<p> Received <p> $message <p> from $emailFrom\n";
+if ( mail( $emailTo, "Email Subject : Test", $message, "From: $emailFrom" ) ) {
+    echo "<p> Congratulations your email has been sent to $emailTo";
 }
 else{
     echo "<p> Sorry your email can not be sent";
