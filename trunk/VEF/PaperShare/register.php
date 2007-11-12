2c412bbe03dd4712bf8f4382d9a8c0d946a1142d
hoangmanhquang <hoangmanhquang@2dacef61-802d-0410-83c8-2ff38f27e8b0>
2007-11-12 20:40:28 +0000

    Modify UI

--- trunk/VEF/PaperShare/register.php
+++ trunk/VEF/PaperShare/register.php
@@ -1,6 +1,7 @@
 <?php
 include "chk_login.inc";
 ?>
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Đăng ký thành viên</title>
@@ -89,7 +90,6 @@ return true;
 
 <link href="Theme/Default/style.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
 <table width="999" border="0" align="center" bgcolor="#FFFFFF" >
   <tr bgcolor="#CCCC66">
