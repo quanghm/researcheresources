91d45c28ef7b7ee0faac1313fe27e8435387593f
kimcuong <kimcuong@2dacef61-802d-0410-83c8-2ff38f27e8b0>
2007-11-23 05:13:44 +0000

    delete redundant directory

--- trunk/VEF/PaperShare/paper_share/mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'config.php';
-include $strIncDir."sendmail/mail.php";
-$emlTo = "qhoang@princeton.edu";
-$Subject = "Khong tim duoc bai bao cua ban";
-$message = "Test message";
-print do_send($emlTo,"Quang Hoang", $Subject, $message);
-?>
