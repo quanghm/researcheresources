469747dd6f1a29135f0f2dfb9c1f98e7bc1b8869
kimcuong <kimcuong@2dacef61-802d-0410-83c8-2ff38f27e8b0>
2008-08-15 21:43:56 +0000

    added list views, clean up css

--- trunk/django/ncs/papershare/models.py
+++ trunk/django/ncs/papershare/models.py
@@ -6,18 +6,55 @@ from django.contrib.auth.models import User
 class Announcement(models.Model):
     content = models.TextField()
     date = models.DateField()
-
-#extend user with a profile
-# see http://www.b-list.org/weblog/2006/jun/06/django-tips-extending-user-model/
+    def __unicode__(self):
+        return "Announcement : %s" % self.content[:20]
+    
 RESEARCH_FIELDS = (
         ('BIO', 'Biology'),
         ('CHEM', 'Chemistry'),
+        ('CS', 'Computer Science'),
         ('EE', 'Electrical Engineering'),
         ('ME', 'Mechanical Engineering'),
         ('MATH', 'Mathematics'),
         ('PHYS', 'Physics'),
         ('NONE', 'None of the above'),
     )
+
+class Paper(models.Model):
+    link = models.URLField()
+    title = models.CharField(max_length = 255)
+    author = models.CharField(max_length = 255)
+    publisher = models.CharField(max_length = 255)
+    year = models.IntegerField()
+    issue = models.IntegerField()
+    page = models.IntegerField()
+    research_field = models.CharField(max_length=4, choices = RESEARCH_FIELDS);
+    local_link = models.URLField()
+    
+    def __unicode__(self):
+        return "Paper : %s" % self.title[:20]
+    
+REQUEST_STATUS_CHOICES = (
+        (0, "pending"),
+        (1, "assigned"),
+        (2, "re-assigned"),
+        (3, "supplied"),
+        (4, "thanked"),
+    )
+
+class Request(models.Model):
+    paper = models.ForeignKey(Paper, related_name = "paper to request")
+    date_requested = models.DateField()
+    date_assigned = models.DateField(null = True)
+    requester = models.ForeignKey(User, related_name = "paper requester")
+    supplier =  models.ForeignKey(User, related_name = "paper supplier", null = True)
+    status = models.SmallIntegerField(choices = REQUEST_STATUS_CHOICES)
+    
+    def __unicode__(self):
+        return "[%d] Request for paper %d" % (self.id,self.paper.id)
+    
+#extend user with a profile
+# see http://www.b-list.org/weblog/2006/jun/06/django-tips-extending-user-model/
 class PaperShareProfile(models.Model):
     # This is the only required field
     user = models.ForeignKey(User, unique=True)
@@ -26,6 +63,9 @@ class PaperShareProfile(models.Model):
     # The rest is completely up to you...
     research_field = models.CharField(max_length=4, choices = RESEARCH_FIELDS);
     is_supplier = models.BooleanField()
+    
+    def __unicode__(self):
+        return "Profile for %d" % self.user.id
 
 def paper_share_profile_callback(user, research_field, is_supplier):
     print "--- HAHA"
